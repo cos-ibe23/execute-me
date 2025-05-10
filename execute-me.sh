0d98f1067626a5def7572e9c4234ef3c83b32f17
cos-ibe23 <cosibeanointed7@gmil.com>
2025-05-10 21:41:48 +0100

    aaded command to check for OS type linux-gnu.

--- execute-me.sh
+++ execute-me.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 if ! command -v zenity &> /dev/null; then
-    echo "zenity is not installed, lol, installing with Homebrew...."
-    
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew is not installed. Please install Homebrew first: https://brew.sh"
-        exit 1
-    fi
+    echo "zenity is not installed. Attempting to install..."
 
-    brew install zenity
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if ! command -v apt &> /dev/null; then
+            echo "APT not found. Cannot install zenity. Please install manually."
+            exit 2
+        fi
+
+        sudo apt update && sudo apt install -y zenity
+
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        if ! command -v brew &> /dev/null; then
+            echo "Homebrew is not installed. Please install Homebrew first: https://brew.sh"
+            exit 2
+        fi
+
+        brew install zenity
+    else
+        echo "Unsupported OS. Please install zenity manually."
+        exit 2
+    fi
 fi
 
 while true; do
